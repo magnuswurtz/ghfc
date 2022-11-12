b2556e6b32dd123c28466e1bb43ce8d3533000bc
Magnus Würtz <m92@live.dk>
2022-11-12 09:18:22 +0100

    add new screen files

---
+++ screens/MapScreen.tsx
@@ -0,0 +1,26 @@
+import React, {type PropsWithChildren,useState} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import MapView from 'react-native-maps';
+
+const MapScreen = () => {
+  return (
+    <MapView
+    initialRegion={{
+      latitude: 37.78825,
+      longitude: -122.4324,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    }}
+  />
+  )
+}
+
+export default MapScreen;
---
+++ screens/NavigationScreen.tsx
@@ -0,0 +1,19 @@
+import Arrow from '@elsdoerfer/react-arrow';
+import React, {type PropsWithChildren,useState} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  
+} from 'react-native';
+
+const Navigation = () => {
+    return(
+        <Text>Navigation</Text>
+    )
+}
+export default Navigation; 
